dc806cce33e52f6bfe6acd4944110c89fa222a28
Kaik Dorvalo <88159821+kaikdorvalo@users.noreply.github.com>
2024-06-05 22:21:15 -0300

    initial structure

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { BooksModule } from './books/books.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
+import { ProductsModule } from './products/products.module';
+import { ProductPriceMiddleware } from './products/middlewares/productPrice.middleware';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://0.0.0.0/books-nest'),
+    BooksModule,
+    ProductsModule,
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('cats');
+
+    consumer
+      .apply(ProductPriceMiddleware)
+      .forRoutes({ path: 'products', method: RequestMethod.POST })
+  }
+}
