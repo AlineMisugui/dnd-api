30cb3b11d37a46277f358285868b6308e625bf98
Velinu <120288903+Velinu@users.noreply.github.com>
2024-06-22 12:40:39 -0300

    Feature/auth (#4)
    * crud_user
    
    * dto_separate
    
    * user_conected_to_database
    
    * feat(auth): auth implementation
    
    * chore: Remove trailing whitespace in users.service.ts
    
    ---------
    
    Co-authored-by: Emerson Tanno <tannobrizotto@gmail.com>

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -13,10 +14,11 @@ export class AuthService {
         username: string,
         pass: string,
     ): Promise<{ access_token: string }> {
-        const user = await this.usersService.findOne(username);
-       /* if (user?.password !== pass) {
+        const user = await this.usersService.loginFind(username);
+        const isMatch = await bcrypt.compare(pass, user.password);
+        if (!isMatch) {
             throw new UnauthorizedException();
-        } */
+        }
         const payload = { sub: user.userId, username: user.username };
         return {
             access_token: await this.jwtService.signAsync(payload),
