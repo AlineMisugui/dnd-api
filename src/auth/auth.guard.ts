1558f6ddd28336910b4c93f2e70f0fa86772ba68
Velinu <matheusfelintosilva2@gmail.com>
2024-06-22 15:12:47 -0300

    feat(auth): public impl

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -7,14 +7,21 @@ import {
 import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './auth.controller';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private jwtService: JwtService) { }
+    constructor(private jwtService: JwtService, private reflector: Reflector) { }
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const request = context.switchToHttp().getRequest();
         const token = this.extractTokenFromHeader(request);
+        
+        if (this.checkIfPublic(context)) {
+            return true;
+        }
+        
         if (!token) {
             throw new UnauthorizedException();
         }
@@ -25,8 +32,6 @@ export class AuthGuard implements CanActivate {
                     secret: jwtConstants.secret
                 }
             );
-            // 💡 We're assigning the payload to the request object here
-            // so that we can access it in our route handlers
             request['user'] = payload;
         } catch {
             throw new UnauthorizedException();
@@ -34,8 +39,18 @@ export class AuthGuard implements CanActivate {
         return true;
     }
 
+    private checkIfPublic(context: ExecutionContext): boolean {
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+
+        return isPublic ? true : false;
+    }
+
     private extractTokenFromHeader(request: Request): string | undefined {
         const [type, token] = request.headers.authorization?.split(' ') ?? [];
         return type === 'Bearer' ? token : undefined;
     }
 }
+
